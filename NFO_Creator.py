1e04111372a747c28b56dd31d556f3218d4dddf7
stewiegreen <53995844+stewiegreen@users.noreply.github.com>
2019-09-26 23:14:00 +1000

    Error handling with the date check

--- NFO_Creator.py
+++ NFO_Creator.py
@@ -11,17 +11,20 @@ TI = sg.TeamInfo(league)
 teams = TI.get_teams(league)
 
 
-input_date = input("Which date did the game occur? (yyyy-mm-dd)")
+while True:
+    input_date = input("Which date did the game occur? (yyyy-mm-dd)")
 
-date = sg.Date(input_date)
-datecheck = date.date_checker()
+    date = sg.Date(input_date)
+    datecheck = date.date_checker()
 
 
-if datecheck:
-    print("Thank you, generating file")
-    game_day = TI.date_played(input_date, teams)
-    TI.create_nfo(game_day)
+    if not datecheck:
+        print("Please enter a valid date format")
+    
+    if datecheck:
+        print("Thank you, generating file")
+        game_day = TI.date_played(input_date, teams)
+        TI.create_nfo(game_day)
+        break
 
-else:
-    print("Invalid Date format")
-
+    
