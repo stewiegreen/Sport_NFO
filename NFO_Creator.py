73022ea344565f4303d2bc3044dd165f92f408d5
stewiegreen <53995844+stewiegreen@users.noreply.github.com>
2019-09-27 14:56:58 +1000

    added an easy way to get the last game played

--- NFO_Creator.py
+++ NFO_Creator.py
@@ -1,4 +1,3 @@
-import json, requests, sys, os, pprint 
 import sportgrabber as sg
 
 
@@ -8,23 +7,29 @@ league = sg.LeagueInfo.get_leagues()
 
 TI = sg.TeamInfo(league)
 
-teams = TI.get_teams(league)
+team = TI.get_teams(league)
 
 
 while True:
-    input_date = input("Which date did the game occur? (yyyy-mm-dd)")
+    input_date = input("Which date did the game occur? (yyyy-mm-dd)" + "\n" + "Leave blank for latest game :")
 
+    if input_date == "":
+        game_day = TI.get_last_game(team)
+        TI.create_nfo(game_day)
+        break
+    
     date = sg.Date(input_date)
     datecheck = date.date_checker()
-
 
     if not datecheck:
         print("Please enter a valid date format")
     
     if datecheck:
         print("Thank you, generating file")
-        game_day = TI.date_played(input_date, teams)
+        game_day = TI.date_played(input_date, team)
         TI.create_nfo(game_day)
         break
 
     
+    else:
+        print("Sorry, something went wrong.")
